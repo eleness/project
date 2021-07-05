3211702da834345f8dc92a69b74abdb7778e6aed
engir <work.home7@bk.ru>
2021-07-05 15:16:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@
     res.send('Тест');
   });
 
+  app.get('/*', (req, res) => res.sendFile('./dist/index.html', { root: __dirname }));
+
   // Слушаем порт и при запуске сервера сообщаем
   app.listen(setup.port, () => {
     console.log('Сервер: порт %s - старт!', setup.port);
