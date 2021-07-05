5602ba8fca0e76c52b769bcec47b393722c6f29d
engir <work.home7@bk.ru>
2021-07-05 22:44:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
   const app = express ();
 
   app.get('/*', (req, res) => res.sendFile('./dist/index.html', { root: __dirname }));
-  app.get('/api', (req, res) => {
+  app.get('/api/news', (req, res) => {
     var getAnimeNews = require('./controller');
     getAnimeNews.then(
       results => {
